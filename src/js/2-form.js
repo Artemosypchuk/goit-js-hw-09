92185106b9e3f90ce6f9b7eabbcbfadcc8fc8415
tyoska <tyoska111@ukr.net>
2026-05-11 12:49:29 +0300

    HW-JS-9

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,47 @@
 
+let formData = {
+    email: "",
+    message: ""
+};
+
+const form = document.querySelector(".feedback-form");
+const STORAGE_KEY = "feedback-form-state";
+
+populateForm();
+
+form.addEventListener("input", (event) => {
+    const { name, value } = event.target;
+    
+    formData[name] = value.trim();
+    
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const { email, message } = formData;
+
+    if (email === "" || message === "") {
+        alert("Fill please all fields");
+        return;
+    }
+
+    console.log(formData);
+
+    localStorage.removeItem(STORAGE_KEY);
+    formData = { email: "", message: "" };
+    form.reset();
+});
+
+function populateForm() {
+    const savedData = localStorage.getItem(STORAGE_KEY);
+
+    if (savedData) {
+        const parsedData = JSON.parse(savedData);
+        formData = parsedData;
+        Object.entries(parsedData).forEach(([key, value]) => {
+            form.elements[key].value = value;
+        });
+    }
+}
